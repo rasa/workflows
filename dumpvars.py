aa1fa5c272cc0a19c2d4fff2470e3fb5c8806c09
Ross Smith II <ross@smithii.com>
2023-08-27 14:20:08 -0700

    Update dumpvars.py

--- dumpvars.py
+++ dumpvars.py
@@ -5,12 +5,16 @@ import os
 import pprint
 import sys
 
-print(f"os.name={os.name}")
-print(f"sys.executable={sys.executable}")
+os_name = os.name
+print(f"os.name={os_name}")
+sys_executable = sys.executable
+print(f"sys.executable={sys_executable}")
 print("sys.argv=")
 pprint.pprint(sys.argv)
-print(f"sys.version={sys.version}")
-print("sys.platform=sys.platform}")
+sys_version = sys.version
+print(f"sys.version={sys_version}")
+sys_platform = sys.platform
+print("sys.platform={sys_platform}")
 print("os.environ=")
 pprint.pprint(dict(os.environ), width=4096)
 sys.exit(0)
